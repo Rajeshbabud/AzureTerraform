acabae25dd4815d3aa37b0d597aabf5398cb224f
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-04-07 11:00:40 -0500

    removed resource group

--- main.tf
+++ main.tf
@@ -3,11 +3,6 @@ provider "azurerm" {
   version         = "~> 2.4.0"
   features {}
 }
-resource "azurerm_resource_group" "app-gw-poc" {
-  name     = var.resource-group-name
-  location = var.resource-group-location
-}
-
 module "agw-module-appservice" {
   source = "./appservice"
   resource-group-location                                 = var.resource-group-location
