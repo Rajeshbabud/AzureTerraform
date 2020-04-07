f547291f980e3445dc06ed6393fc392b3b2036a7
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-04-07 14:37:31 -0500

    trying modules

--- main.tf
+++ main.tf
@@ -3,8 +3,13 @@ provider "azurerm" {
   version         = "~> 2.4.0"
   features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+    name     = "app-gw-poc"
+    location = var.resource-group-location
+}
 module "agw-module-appservice" {
   source = "./appservice"
   resource-group-location                                 = var.resource-group-location
-  resource-group-name                                     = var.resource-group-name
+  resource-group-name                                     = azurerm_resource_group.rg.name
 }
