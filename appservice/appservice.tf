8b03648b2caf0283124ab498d5774db69854c43a
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-04-07 15:19:35 -0500

    updated subnet name

--- appservice/appservice.tf
+++ appservice/appservice.tf
@@ -12,10 +12,11 @@ resource "azurerm_app_service_plan" "poc" {
 }
 
 resource "azurerm_app_service" "poc-app" {
-  name                = "agw-appservice-poc"
-  location            = var.resource-group-location
-  resource_group_name = var.resource-group-name
-  app_service_plan_id = azurerm_app_service_plan.poc.id
+  name                     = "agw-appservice-poc"
+  location                 = var.resource-group-location
+  resource_group_name      = var.resource-group-name
+  app_service_plan_id      = azurerm_app_service_plan.poc.id
+  virtual_network_name     = var.vnet_name
   site_config {
     linux_fx_version = "JAVA|11-java11"
     java_version = "11"
