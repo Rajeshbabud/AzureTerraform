2743105d25f84343b2bd15b65ea83d03b2a9792c
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-04-07 15:09:14 -0500

    updated subnet name

--- vnet/output-vnet.tf
+++ vnet/output-vnet.tf
@@ -9,10 +9,6 @@ output "vnet_address_space" {
   value       = azurerm_virtual_network.vnet.address_space
 }
 
-output "vnet_subnets" {
-  description = "The ids of subnets created inside the newl vNet"
-  value       = azurerm_subnet.subnet1.id
-}
 output "vnet_name" {
   description = "The Name of the newly created vNet"
   value       = azurerm_virtual_network.vnet.name
