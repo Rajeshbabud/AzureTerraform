3f3f3ec9966c9117fbfed7cb2a9a8512616816c4
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-04-08 16:19:27 -0500

    added VM

---
+++ vm/variables-vm.tf
@@ -0,0 +1,11 @@
+variable "resource-group-location" {
+  description ="Resource Group location. Possible value is East US."
+}
+
+variable "resource-group-name" {
+  description ="Resource Group name."
+}
+
+variable "subnet_frontend_id" {
+  description ="Subnet Id"
+}
