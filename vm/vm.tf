220ab89882b3b23d91fc57113dbecf82a0d14f80
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-04-08 16:34:42 -0500

    added VM

--- vm/vm.tf
+++ vm/vm.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  name                = "raj-nic"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "testconfiguration1"
