3a06f62ed04b3794d00184cf960316f2ee60becb
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-05-07 11:54:04 -0500

    added alerts

--- main.tf
+++ main.tf
@@ -22,25 +22,25 @@ module "alerts" {
   app-service-plan-poc-id                                 = module.appservice.app-service-plan-poc-id
 }
 
-module "vnet" {
-  source = "./vnet"
-  resource-group-location                                 = var.resource-group-location
-  resource-group-name                                     = azurerm_resource_group.rg.name
-}
+#module "vnet" {
+#  source = "./vnet"
+#  resource-group-location                                 = var.resource-group-location
+#  resource-group-name                                     = azurerm_resource_group.rg.name
+#}
 
-module "vm" {
-  source = "./vm"
-  resource-group-location                                 = var.resource-group-location
-  resource-group-name                                     = azurerm_resource_group.rg.name
-  subnet_vm_id                                            = module.vnet.subnet_vm_id
-}
+#module "vm" {
+#  source = "./vm"
+#  resource-group-location                                 = var.resource-group-location
+#  resource-group-name                                     = azurerm_resource_group.rg.name
+#  subnet_vm_id                                            = module.vnet.subnet_vm_id
+#}
 
-module "app-gateway" {
-  source = "./appgw"
-  resource-group-location                                 = var.resource-group-location
-  resource-group-name                                     = azurerm_resource_group.rg.name
-  subnet_frontend_id                                      = module.vnet.subnet_frontend_id
-  vnet_name                                               = module.vnet.vnet_name
-  rajvent_pip_id                                          = module.vnet.rajvent_pip_id 
-}
+#module "app-gateway" {
+#  source = "./appgw"
+#  resource-group-location                                 = var.resource-group-location
+#  resource-group-name                                     = azurerm_resource_group.rg.name
+#  subnet_frontend_id                                      = module.vnet.subnet_frontend_id
+#  vnet_name                                               = module.vnet.vnet_name
+#  rajvent_pip_id                                          = module.vnet.rajvent_pip_id 
+#}
 
