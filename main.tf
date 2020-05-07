864c4282c9f95eb2312d74705aa7d43818b92223
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-05-07 11:51:27 -0500

    added alerts

--- main.tf
+++ main.tf
@@ -19,7 +19,28 @@ module "alerts" {
   source = "./alerts"
   resource-group-location                                 = var.resource-group-location
   resource-group-name                                     = azurerm_resource_group.rg.name
+  app-service-plan-poc-id                                 = module.appservice.app-service-plan-poc-id
 }
 
+module "vnet" {
+  source = "./vnet"
+  resource-group-location                                 = var.resource-group-location
+  resource-group-name                                     = azurerm_resource_group.rg.name
+}
 
+module "vm" {
+  source = "./vm"
+  resource-group-location                                 = var.resource-group-location
+  resource-group-name                                     = azurerm_resource_group.rg.name
+  subnet_vm_id                                            = module.vnet.subnet_vm_id
+}
+
+module "app-gateway" {
+  source = "./appgw"
+  resource-group-location                                 = var.resource-group-location
+  resource-group-name                                     = azurerm_resource_group.rg.name
+  subnet_frontend_id                                      = module.vnet.subnet_frontend_id
+  vnet_name                                               = module.vnet.vnet_name
+  rajvent_pip_id                                          = module.vnet.rajvent_pip_id 
+}
 
