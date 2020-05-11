0813bef08e5620e7c32e4c39019d7a11281a9e04
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-05-11 10:40:22 -0500

    added output file

--- alerts/alerts.tf
+++ alerts/alerts.tf
@@ -61,29 +61,3 @@ resource "azurerm_monitor_metric_alert" "memory-alert" {
   }
 } 
 
-resource "azurerm_monitor_scheduled_query_rules_alert" "example" {
-  name                = "Rajesh-custom-query"
-  location            = var.resource-group-location
-  resource_group_name = var.resource-group-name
-
-  action {
-    action_group           = [azurerm_monitor_action_group.raj-actiongroup.id]
-    email_subject          = "Email Header"
-    custom_webhook_payload = "{}"
-  }
-  data_source_id = var.appinsights_id
-  description    = "Alert when total results cross threshold"
-  enabled        = true
-  # Count all requests with server error result code grouped into 5-minute bins
-  query       = <<-QUERY
-  traces
-    | where message contains "Exception" and cloud_RoleInstance == "aa-oh-test-appservice-ccs"  
-  QUERY
-  severity    = 1
-  frequency   = 5
-  time_window = 30
-  trigger {
-    operator  = "GreaterThan"
-    threshold = 3
-  }
-}
