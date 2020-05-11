deba7fbe502d8937b0eac283208120e17fa3b820
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-05-11 14:51:50 -0500

    added a new appservice plan

--- appservice/output-appservice.tf
+++ appservice/output-appservice.tf
@@ -1,11 +1,17 @@
 output "app-service-plan-poc-id" {
   value = azurerm_app_service_plan.poc.id
-  description = "The apis app service plan id."
+  description = "The poc app service plan id."
+  sensitive = true
+}
+
+output "app-service-plan-test-id" {
+  value = azurerm_app_service_plan.test.id
+  description = "The test app service plan id."
   sensitive = true
 }
 
 output "app-service-poc-id" {
   value = azurerm_app_service.poc-app.id
-  description = "The apis app service id."
+  description = "The poc app service id."
   sensitive = true
 }
