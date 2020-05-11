7adddc9850281449161fb3b23c1b7463902a6229
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-05-11 10:01:33 -0500

    added output file

---
+++ alerts/output-alerts.tf
@@ -0,0 +1,7 @@
+output "actiongroup-name" {
+  value = azurerm_monitor_action_group.raj-actiongroup.name
+}
+
+output "actiongroup-id" {
+  value = azurerm_monitor_action_group.raj-actiongroup.id
+}
