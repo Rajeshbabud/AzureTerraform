245b236e58b8b9634c0e7770e222b8fe3818eb70
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-05-12 09:36:51 -0500

    Added Ehub and alerts

--- eventhubs/output-eventhub.tf
+++ eventhubs/output-eventhub.tf
@@ -1,4 +1,4 @@
-output "eventhubs-internal-id" {
+output "eventhubs-raj-id" {
   value = azurerm_eventhub_namespace.raj-eventhub-namespace.id
   description = "internal eventshub namespace ID."
   sensitive = true
