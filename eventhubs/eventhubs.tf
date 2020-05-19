e8a4cdf368e220a5cc712a081a3346aa6ea7f83f
Rajesh Devarapalli <Rajeshbabu.Devarapalli@aa.com>
2020-05-19 13:18:30 -0500

    fixed issues in loganalytics

--- eventhubs/eventhubs.tf
+++ eventhubs/eventhubs.tf
@@ -1,5 +1,5 @@
 resource "azurerm_eventhub_namespace" "raj-eventhub-namespace" {
-  name                = "eventhub-raj"
+  name                = "raj-oh-eventhub"
   location            = var.resource-group-location
   resource_group_name = var.resource-group-name
   sku                 = "Standard"
